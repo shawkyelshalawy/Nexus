0af7381b5a77e387d2f1d91d0aa45b38d04fde04
Shawky Elshalawy <85047354+shawkyelshalawy@users.noreply.github.com>
2024-04-03 07:46:20 +0200

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -19,39 +19,43 @@ const (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		log.Println("Failed to bind to port 4221")
 		os.Exit(1)
+
 	}
+	// Uncomment this block to pass the first stage
 	defer func() {
 		err := l.Close()
 		if err != nil {
 			log.Println("Error closing listener: ", err.Error())
 			return
 		}
+
 	}()
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Failed to bind to port 4221")
+		log.Println("Error accepting connection: ", err.Error())
+
 		os.Exit(1)
 	}
+	_, err = l.Accept()
 	defer func() {
 		err := conn.Close()
 		if err != nil {
 			log.Println("Error closing connection: ", err.Error())
 			return
 		}
-	}()
 
+	}()
 	okResponse := httpProtocol + "/" + httpVersion + " " + httpStatusOK + " " + httpVersion + crlf
 	_, err = conn.Write([]byte(okResponse))
 	if err != nil {
-		fmt.Println("Failed to write to connection", err.Error())
+		fmt.Println("Error accepting connection: ", err.Error())
+		log.Println("Error writing response: ", err.Error())
 		os.Exit(1)
-	}
 
+	}
 }
